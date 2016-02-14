dcb7872ec7775af09fe5f5170617dfc3ac91fdfb
Luigi <kakitatus@gmail.com>
2016-02-14 19:40:06 +0100

    added if

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -7,10 +7,14 @@ public class Hello
 
     public static void main(String[] args)
     {
-	System.out.print("Hello World!!");
-	System.out.println("GitHub ist tol");
-	System.out.println("Ja das Stimmt");
-	int i = 0;
+		System.out.print("Hello World!!");
+		System.out.println("GitHub ist tol");
+		System.out.println("Ja das Stimmt");
+		if(true){
+		    return;
+		}
+		int i = 0;
     }
+    
 
 }
